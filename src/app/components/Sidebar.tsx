0cdac1d1b6c3aa31bac42d7b9c1360802b8b5e0d
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-05-08 21:32:00 +0300

    work with animation

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import { NavLinks } from '../constants';
+import Link from 'next/link';
+import Transition from './Transition';
+
+const Sidebar = () => {
+  const [isRouting, setIsRouting] = useState(false);
+  const [isActive, setIsActive] = useState('Home');
+  const [prevPath, setPrevPath] = useState('/');
+
+  const path = usePathname();
+
+  const links = NavLinks.map((link) => (
+    <Link
+      key={link.name}
+      href={link.link}
+      onClick={() => setIsActive(link.name)}
+    >
+      <link.icon
+        className={`w-[28px] h-[28px] ${
+          isActive === link.name ? 'text-orange-500' : 'text-white'
+        }`}
+      />
+    </Link>
+  ));
+
+  useEffect(() => {
+    if (prevPath !== path) {
+      setIsRouting(true);
+    }
+  }, [path, prevPath]);
+
+  useEffect(() => {
+    if (isRouting) {
+      setPrevPath(path);
+      const timeout = setTimeout(() => {
+        setIsRouting(false);
+      }, 1200);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [path, isRouting]);
+
+  return (
+    <div className="fixed right-8 top-[40%] z-[20] h-[200px] w-[48px] rounded-full bg-gray-500 bg-opacity-50">
+      {isRouting && <Transition />}
+      <AnimatePresence mode="wait">
+        <div className="flex flex-col gap-5 justify-center items-center pb-3 h-full">
+          {links}
+        </div>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Sidebar;
